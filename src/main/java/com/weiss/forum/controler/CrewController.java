0c54de78de32002dc3c587f715458d0a886e4296
Niklas Weiss <nikiathome@web.de>
2016-11-11 23:10:19 +0100

    functional add crew

--- src/main/java/com/weiss/forum/controler/CrewController.java
+++ src/main/java/com/weiss/forum/controler/CrewController.java
@@ -1,11 +1,16 @@
 package com.weiss.forum.controler;
 
+import com.weiss.forum.db.repository.CrewRepository;
 import com.weiss.forum.logic.ContentController;
+import com.weiss.forum.logic.CrewContent;
 import java.math.BigInteger;
+import java.util.logging.LogManager;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,6 +21,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 public class CrewController {
+
+    @Autowired
+    private CrewRepository crewRepository;
+
+    public static Logger LOGGER = Logger.getLogger(CrewController.class);
 
     @Autowired
     private ContentController contentController;
@@ -49,17 +59,20 @@ public class CrewController {
 	model.addAttribute("content", this.contentController.getContents("crew"));
 	model.addAttribute("leftNavigation", this.contentController.getLeftNavigation("ger"));
 	model.addAttribute("rightNavigation", this.contentController.getRightNaviation("ger"));
+	model.addAttribute("crew", new CrewContent());
 	return "index";
     }
 
     @RequestMapping(value = "/crew/add", method = RequestMethod.POST)
     @PreAuthorize("hasRole('ADMIN')")
-    public String addPost(ModelMap model) {
-	model.addAttribute("title", "Bikepark Herzogenrath");
-	model.addAttribute("site", "crewAdd");
-	model.addAttribute("content", this.contentController.getContents("crew"));
-	model.addAttribute("leftNavigation", this.contentController.getLeftNavigation("ger"));
-	model.addAttribute("rightNavigation", this.contentController.getRightNaviation("ger"));
-	return "index";
+    public String addPost(ModelMap model, @ModelAttribute("crew") CrewContent crew) {
+	LOGGER.info(crew.getHeader());
+	LOGGER.info(crew.getBody());
+	LOGGER.info(crew.getFooter());
+	LOGGER.info(crew.getImage());
+	
+	crewRepository.save(crew);
+
+	return "redirect:/crew";
     }
 }
--- src/main/webapp/WEB-INF/jsp/pages/crewAdd.jsp
+++ src/main/webapp/WEB-INF/jsp/pages/crewAdd.jsp
@@ -5,32 +5,33 @@
 --%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@ taglib prefix="sec" uri="http://www.springframework.org/security/tags" %>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form"%>
 <div class="col-xs-12 col-sm-6 col-lg-4">
     <sec:authorize access="hasRole('ADMIN')">
-	<form action="/crew/add/" method="POST">
+	<form:form action="/crew/add/" method="POST" modelAttribute="crew">
 	    <input type="hidden" name="${_csrf.parameterName}" value="${_csrf.token}"/>
 	    <input type="hidden" value="${test.getId()}" name="id" />
 	    <div class="form-group">
-		<label for="header">Header</label>
-		<input type="text" class="form-control" id="header" />
+		<form:label path="header">Header</form:label>
+		<form:input path="header" />
 	    </div>
 	    <div class="form-group">
-		<label for="body">Body</label>
-		<input type="text" class="form-control" id="body" />
+		<form:label path="body">Body</form:label>
+		<form:input path="body" />
 	    </div>
 	    <div class="form-group">
-		<label for="footer">Footer</label>
-		<input type="text" class="form-control" id="footer" />
+		<form:label path="footer">Footer</form:label>
+		<form:input path="footer" />
 	    </div>
 	    <div class="form-group">
-		<label for="image">Bild</label>
-		<input type="text" class="form-control" id="image" />
+		<label path="image">Bild</label>
+		<form:input path="image" />
 	    </div>
 
 	    <button type="button submit" class="btn btn-default btn-lg">
 		<span class="glyphicon glyphicon-pencil" aria-hidden="true" />
 		Hinzufügen
 	    </button>
-	</form>
+	</form:form>
     </sec:authorize>
 </div>
