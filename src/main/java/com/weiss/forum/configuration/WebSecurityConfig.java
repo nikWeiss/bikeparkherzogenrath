eadd62b473c6564768933d6972f9acf61eae8610
Niklas Weiss <nikiathome@web.de>
2017-02-27 00:36:40 +0100

    Updating the security of the passwords

--- src/main/java/com/weiss/forum/configuration/WebSecurityConfig.java
+++ src/main/java/com/weiss/forum/configuration/WebSecurityConfig.java
@@ -29,6 +29,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     private PasswordEncoder passwordEncoder;
 
     @Bean
+    public PasswordEncoder passwordEncoder() {
+	return new BCryptPasswordEncoder();
+    }
+
+    @Bean
     public MongoUserDetailsService getService() {
 	return new MongoUserDetailsService();
     }
